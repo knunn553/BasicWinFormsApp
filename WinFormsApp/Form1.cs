4067a1e1ca1938687b7f14d337055a140b6d2b75
knunn553 <knunn553@outlook.com>
2023-12-03 23:16:57 -0500

    Sending data to MSSQL Server through Stored Procedures. This methods prevents SQL injection.

--- WinFormsApp/Form1.cs
+++ WinFormsApp/Form1.cs
@@ -49,17 +49,33 @@ namespace WinFormsApp
             {
                 conn.Close();
             } */
-            
-            System.Data.SqlClient.SqlConnection conn = new System.Data.SqlClient.SqlConnection(@"Data Source=KYLENUNND4C4;Initial Catalog=SensorSystems;Integrated Security=True");
-            string sqlQuery = "INSERT INTO SENSOR (SensorName, SensorType) VALUES (" + "'" + sensorNameValue.Text + "'" + "," + "'" + sensorTypeValue.Text + "'" + ")";
-            
 
-            conn.Open();
-            System.Data.SqlClient.SqlCommand sc = new System.Data.SqlClient.SqlCommand(sqlQuery, conn);
-           
+
+            // string sqlQuery = "INSERT INTO SENSOR (SensorName, SensorType) VALUES (" + "'" + sensorNameValue.Text + "'" + "," + "'" + sensorTypeValue.Text + "'" + ")";
+            try
+            {
+                System.Data.SqlClient.SqlConnection conn = new System.Data.SqlClient.SqlConnection(@"Data Source=KYLENUNND4C4;Initial Catalog=SensorSystems;Integrated Security=True");
+
+
+                conn.Open();
+
+                System.Data.SqlClient.SqlCommand cmd = new System.Data.SqlClient.SqlCommand("SaveSensor", conn);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                string sensorName = sensorNameValue.Text;
+                string sensorType = sensorTypeValue.Text;
+
+                cmd.Parameters.Add(new System.Data.SqlClient.SqlParameter("@SensorName", sensorName));
+                cmd.Parameters.Add(new System.Data.SqlClient.SqlParameter("@SensorType", sensorType));
+
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+            catch
+            {
+                MessageBox.Show("There has been an error. We are rapidly resolving this issue and we will notify you via email when this error is resolved.");
+            }
             
-            sc.ExecuteNonQuery();
-            conn.Close();
 
 
             // Above in the connection string, we have set the TrustServerCertificate to true, but this is bad practice.
